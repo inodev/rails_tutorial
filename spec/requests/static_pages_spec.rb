72b65ece78607482d4b29086e71d5a340b2ff0a0
Inoue Shinya <inosdevelopment@gmail.com>
2014-07-03 22:14:46 +0900

    振舞駆動開発の為のテスト環境改善

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,15 +1,23 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  let(:base_title){ "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
     it "shold haec the cntent 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
 
-    it "shold haec the title 'Home'" do
+    it "shold have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "shold not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
@@ -19,9 +27,9 @@ describe "StaticPages" do
       expect(page).to have_content('Help')
     end
 
-    it "shold haec the title 'Help'" do
+    it "shold have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -31,9 +39,16 @@ describe "StaticPages" do
       expect(page).to have_content('About Us')
     end
 
-    it "shold haec the title 'About Us'" do
+    it "shold have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact pages" do
+    it "shold have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
